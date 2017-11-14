d023cad23ad0241df03d1c11497fa240be5bacfa
T-102 <T-102@PC260>
2017-11-13 19:38:27 -0600

    13/11/17

---
+++ lenguajes/src/excepciones1/ExcepcionesDos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones1;
+
+/**
+ *
+ * @author T-102
+ */
+public class ExcepcionesDos {
+    public static void main(String [] args) {
+        //NumberFormatException=debido al formtato del dato 
+        //String numerofalso="dos";
+        String numerofalso="2";
+        int valor=Integer.parseInt(numerofalso);//Retira las comillas del string
+    }
+}
---
+++ lenguajes/src/excepciones1/ExcepcionesTres.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones1;
+
+/**
+ *
+ * @author T-102
+ */
+public class ExcepcionesTres {
+    public static void main(String[] args) {
+       
+       //ArrayIndexOutOfBoundsException=Indice fuera del limite del arreglo
+        String x[]={"Hola","Mundo"};
+        System.out.println(x[2]);
+    }
+}
